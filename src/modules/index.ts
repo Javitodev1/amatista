aa5a144baa3c62cce2c9a4d72dd643aa04764dd2
Javier Fuentes I <jftes.dev@outlook.es>
2024-02-12 21:09:38 -0300

    foldrs refactored

---
+++ src/modules/index.ts
@@ -0,0 +1,7 @@
+export { default as AboutPage } from './about/About.astro'
+export { default as CommunityPage } from './community/Comunity.astro'
+export { default as ContactPage } from './contact/Contact.astro'
+export { default as HomePage } from './home/Home.astro'
+export { default as LegalPage } from './legal/Legal.astro'
+export { default as ProductPage } from './product/Product.astro'
+export { default as ShopPage } from './shop/Shop.astro'
