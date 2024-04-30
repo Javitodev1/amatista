acf56f2d2d1cb892be5c3d0bfcb88f527961d148
Javitodev <javito.dev@gmail.com>
2024-04-30 14:20:02 -0400

    removed unused currency formatter

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,9 +1,3 @@
-const formatter = new Intl.NumberFormat("es-UY", {
-  style: "currency",
-  currency: "UYU",
-  maximumFractionDigits: 0,
-})
-
 export function formatCurreny(price: number) {
   return `$ ${price}`
 }
