40750dbc1c7dbc41448c6a15f5aadd5d4916894a
Javitodev <javito.dev@gmail.com>
2024-05-03 16:32:10 -0400

    added responsive title variant

--- src/constants/typographi.ts
+++ src/constants/typographi.ts
@@ -1,5 +1,6 @@
 export enum Variants {
   BANNER = 'text-3xl sm:text-4xl md:text-5xl uppercase font-lora font-bold',
+  RESPONSIVE_TITLE = 'text-2xl sm:text-3xl md:text-4xl uppercase font-lora font-bold text-center md:text-left',
   TITLE = 'text-2xl sm:text-3xl md:text-4xl uppercase font-lora font-bold',
   SUBTITLE = 'text-lg sm:text-xl md:text-2xl uppercase font-lora',
   BODY_TITLE = 'text-lg md:text-xl uppercase font-semibold font-lora',
@@ -8,9 +9,11 @@ export enum Variants {
 
 export enum TextColors {
   ACCENT_AMATISTA = 'text-amatista',
+  ACCENT_GOLDEN = 'text-golden',
   WHITE = 'text-white',
   RAW_WHITE = 'text-raw_white',
   BLACK = 'text-black',
+  RESPONSIVE = "text-black dark:text-white",
   NONE = '',
 }
 
