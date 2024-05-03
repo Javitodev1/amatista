4f1eddf51b28310dcbe4ac1a9d4337e302dfcdb3
Javitodev <javito.dev@gmail.com>
2024-05-02 20:01:37 -0400

    home page mobile version responsive design remade.

--- src/constants/typographi.ts
+++ src/constants/typographi.ts
@@ -2,7 +2,7 @@ export enum Variants {
   BANNER = 'text-3xl sm:text-4xl md:text-5xl uppercase font-lora font-bold',
   TITLE = 'text-2xl sm:text-3xl md:text-4xl uppercase font-lora font-bold',
   SUBTITLE = 'text-lg sm:text-xl md:text-2xl uppercase font-lora',
-  BODY_TITLE = 'text-lg md:text-xl uppercase font-lora',
+  BODY_TITLE = 'text-lg md:text-xl uppercase font-semibold font-lora',
   BODY = 'text-md md:text-lg font-urbanist',
 }
 
